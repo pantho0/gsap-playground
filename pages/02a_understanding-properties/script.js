959254e1d76e41f9b155dc7191b8d7f3601fe3b6
pantho0 <panthomashreky6@gmail.com>
2025-08-15 17:26:16 +0600

    exploring basci properties

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+
+gsap.to(".box", {
+  y: 50,
+  opacity: 1,
+  rotate: 360,
+  //   borderRadius: "50%",
+  scale: 1.1,
+  background: "#ff6f61",
+  duration: 2,
+  ease: "bounce",
+  //   repeat: -1,
+  //   yoyo: true,
+  //   repeatDelay: 2,
+});
